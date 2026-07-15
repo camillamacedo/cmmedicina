55c067d584cad95bcd9b1caeb886f9a179d90985
camillamacedo <millafox@gmail.com>
2026-07-15 13:48:14 -0300

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,1 +1,44 @@
+import { MetadataRoute } from "next";
 
+export default function sitemap(): MetadataRoute.Sitemap {
+  const baseUrl = "https://cmmedicina.com";
+
+  return [
+    {
+      url: baseUrl,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
+    },
+    {
+      url: `${baseUrl}/servicios`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.9,
+    },
+    {
+      url: `${baseUrl}/reservar`,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.9,
+    },
+    {
+      url: `${baseUrl}/contacto`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: `${baseUrl}/Teleconsulta`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: `${baseUrl}/Meet`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.3,
+    },
+  ];
+}
